850c2515fca3ff27994c516254c99f75ac63b5f6
Shido-3 <117528438+Shido-3@users.noreply.github.com>
2024-04-25 13:03:37 -0400

    MK-IV Added some more HTML and CSS Added functionality to text boxes that appear when interacting with certain objects in the map

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,42 @@
 // functions stored here
+
+export function displayDialogue(text, onDisplayEnd) {
+    const dialogueUI = document.getElementById("textbox-container");
+    const dialogue = document.getElementById("dialogue");
+  
+    dialogueUI.style.display = "block"; // "textbox-container" has a property called style, which is currently set to "none" which 
+    // hides a bunch of underlying HTML, by setting it to "block" it reveals the underlying HTML
+
+    // Makes it so that the text is rendered like a video game - character by character
+    let index = 0;
+    let currentText = "";
+    const intervalRef = setInterval(() => {
+      if (index < text.length) {
+        currentText += text[index];
+        dialogue.innerHTML = currentText; // "innerHTML" is bad practice - victim to XSS attacks, but it's okay in our case because we aren't accepting any
+        // user input or sensitive information. This attribute is needed because we need our links to be rendered as links.
+        index++;
+        return;
+      }
+  
+      clearInterval(intervalRef);
+    }, 1);
+  
+    const closeBtn = document.getElementById("close");
+  
+    function onCloseBtnClick() {
+      onDisplayEnd();
+      dialogueUI.style.display = "none";
+      dialogue.innerHTML = "";
+      clearInterval(intervalRef);
+      closeBtn.removeEventListener("click", onCloseBtnClick);
+    }
+  
+    closeBtn.addEventListener("click", onCloseBtnClick);
+  
+    addEventListener("keypress", (key) => {
+      if (key.code === "Enter") {
+        closeBtn.click();
+      }
+    });
+}
