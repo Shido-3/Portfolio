b93c8ecdca67f448691be99639af4f4dd067b1d6
Shido-3 <117528438+Shido-3@users.noreply.github.com>
2024-04-30 22:59:22 -0400

    MK-X-VII Trying again...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/2D-Portfolio/", // Makes it so that the server hosting can read the code
+    base: "/2D-Portfolio", // Makes it so that the server hosting can read the code
     build: {
         minify: "terser", // Code shrinker, makes javascript files smaller in size
     },
