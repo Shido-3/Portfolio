4871dd01efafc027c6f607844747460ad5df86f2
Shido-3 <117528438+Shido-3@users.noreply.github.com>
2024-10-28 18:13:12 -0400

    MK-X-IX-II Updated text to match current/more recent information

--- src/constants.js
+++ src/constants.js
@@ -2,23 +2,22 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     pc: `This is my PC. I work mostly in Python these days, although I'm trying to pick up JavaScript/TypeScript. 
-    Anyway regardless of the language, I just like programming and hope to become a Web Developer someday. Here is my 
+    Anyways regardless of the language, I love programming and hope to become a Software Developer someday. Here is my 
     <a href="https://github.com/Shido-3" target="_blank">Github</a>!`,
-    "cs-degree": `This is where I plan to hang up my CS degree. I'm currently in my 3rd year at Wilfrid Laurier University, 
-    pursuing a BSc in Computer Science with a minor in Economics. I plan to graduate in May 2025. In addition to my 
+    "cs-degree": `This is where I plan to hang up my Bachelor’s degree. I'm currently in my 4th year at Wilfrid Laurier University, 
+    pursuing a BSc in Computer Science with a minor in Economics. I plan to graduate in August 2025. In addition to my 
     academic pursuits, I've also earned a <a href="https://learn.microsoft.com/api/credentials/share/en-us/BenedictHalim/FDE44698A416490A?sharingId=1C42B43509DCF24E" 
     target="_blank">Microsoft Certified: Azure Fundamentals certificate</a>.`,
-    "sofa-table": `This is my sofa. When I'm not working, I like to play video games, read books, watch YouTube, go to 
-    the gym, and hang out with friends. Lately, I've been playing the original Final Fantasy VII.`,
-    tv: `This is my TV. I enjoy watching shows with my mom and dad. We just finished watching Fallout on Prime Video. 
-    Okey dokey!`,
+    "sofa-table": `This is my sofa. When I'm not working, I like to play video games, read books, watch YouTube, run, go to 
+    the gym, and hang out with friends. Lately, I've been playing Final Fantasy VIII.`,
+    tv: `This is my TV. I enjoy watching movies, YouTube and anime. I just finished watching Demon Slayer. Set Your Heart Ablaze!`,
     bed: `This is my bed. I love my bed. I miss my bed.`,
     resume: `This is my desk, where you'll find my resume: here: 
     <a href="https://github.com/Shido-3/Resume/blob/main/Resume.pdf" target="_blank">resume</a>.
-    Contact me at hali4910@mylaurier.ca if you have any interesting job opportunities!`,
+    Contact me at halimbenedict@gmail.com if you have any interesting job opportunities!`,
     projects: `This portfolio was made with the Kaboom.js library, a library for making games in JavaScript. 
     While the portfolio itself is built with Kaboom.js, the text you're reading is created using HTML and CSS.`,
-    library: `I'm currently reading the Dune series. Even though I'm on the second book, I compulsively bought 
+    library: `I'm currently reading the Dune series. Even though I'm on the third book, I compulsively bought 
     the next two in the series. My wallet weeps, but my spice addiction is growing!`,
     exit: `If you want to exit my portfolio, just close the tab. I hope you were able to get a sense of who 
     I am as a person. Have a great day!`,
