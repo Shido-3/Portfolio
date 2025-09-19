a826f5d91ea45059c3da8845a01085cb88b20468
ZackFair <117528438+Shido-3@users.noreply.github.com>
2025-09-19 17:30:31 -0400

    Modified constants.js to reflect recent changes

--- src/constants.js
+++ src/constants.js
@@ -1,24 +1,15 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in Python these days, although I'm trying to pick up JavaScript/TypeScript. 
-    Anyways regardless of the language, I love programming and hope to become a Software Developer someday. Here is my 
-    <a href="https://github.com/Shido-3" target="_blank">Github</a>!`,
-    "cs-degree": `This is where I plan to hang up my Bachelor’s degree. I'm currently in my 4th year at Wilfrid Laurier University, 
-    pursuing a BSc in Computer Science with a minor in Economics. I plan to graduate in August 2025. In addition to my 
-    academic pursuits, I've also earned a <a href="https://learn.microsoft.com/api/credentials/share/en-us/BenedictHalim/FDE44698A416490A?sharingId=1C42B43509DCF24E" 
-    target="_blank">Microsoft Certified: Azure Fundamentals certificate</a>.`,
-    "sofa-table": `This is my sofa. When I'm not working, I like to play video games, read books, watch YouTube, run, go to 
-    the gym, and hang out with friends. Lately, I've been playing Final Fantasy VIII.`,
-    tv: `This is my TV. I enjoy watching movies, YouTube and anime. I just finished watching Demon Slayer. Set Your Heart Ablaze!`,
+    pc: `This is my computer. Lately, I’ve been diving deep into Python and T-SQL—trying to get really good at the latter. No matter the language, I just love programming! Someday I hope to become a Software Developer. If you want to see what I’ve been up to, check out my <a href='https://github.com/Shido-3' target='_blank' rel='noopener noreferrer'>Github</a>!`,
+    "cs-degree": `This is my Bachelor’s degree! I recently graduated from Wilfrid Laurier University with an Honours Bachelor of Science in Computer Science and a Minor in Economics. It took a lot of hard work, but I’m really proud of it. I’ve also earned a <a href='https://learn.microsoft.com/api/credentials/share/en-us/BenedictHalim/FDE44698A416490A?sharingId=1C42B43509DCF24E' target='_blank' rel='noopener noreferrer'>Microsoft Certified: Azure Fundamentals certificate</a>!"`,
+    "sofa-table": `This is my sofa. When I’m not working, you’ll usually find me here — playing games, reading, learning Japanese, going for walks, hitting the gym, or just hanging out with friends. Right now, I’m deep into Final Fantasy VI!`,
+    tv: `This is my TV. I love kicking back with movies, YouTube, and anime. I just wrapped up Demon Slayer — Set Your Heart Ablaze!`,
     bed: `This is my bed. I love my bed. I miss my bed.`,
     resume: `This is my desk, where you'll find my resume: here: 
     <a href="https://github.com/Shido-3/Resume/blob/main/Resume.pdf" target="_blank">resume</a>.
     Contact me at halimbenedict@gmail.com if you have any interesting job opportunities!`,
-    projects: `This portfolio was made with the Kaboom.js library, a library for making games in JavaScript. 
-    While the portfolio itself is built with Kaboom.js, the text you're reading is created using HTML and CSS.`,
-    library: `I'm currently reading the Dune series. Even though I'm on the third book, I compulsively bought 
-    the next two in the series. My wallet weeps, but my spice addiction is growing!`,
-    exit: `If you want to exit my portfolio, just close the tab. I hope you were able to get a sense of who 
-    I am as a person. Have a great day!`,
+    projects: `This portfolio was built using Kaboom.js, a JavaScript library for creating games. The text you’re reading here is rendered with HTML and styled with CSS.`,
+    library: `My bookshelf, my sietch of knowledge. I’m on book four of Dune, and my spice addiction is no longer a joke. Fear is the mind-killer — but running out of reading material might be worse.`,
+    exit: `You can exit anytime by closing this tab — no boss fight required. Thanks for visiting! Hopefully you got to know me a little better. Wishing you a great rest of your day.`,
   };
